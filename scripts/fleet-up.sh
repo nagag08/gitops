22fadfda7557f76daefca1da6f8e4b4ee05292f2
Your Name <you@example.com>
2025-09-21 12:05:44 +0530

    asd

--- scripts/fleet-up.sh
+++ scripts/fleet-up.sh
@@ -17,6 +17,8 @@ addcerts() {
     docker cp ~/Downloads/Z.pem $1:/usr/local/share/ca-certificates/z.crt
     docker exec $1 update-ca-certificates
     docker restart $1
+    sleep 5
+    curl https://asia-south1-docker.pkg.dev/v2/k8s-artifacts-prod/images/ingress-nginx/kube-webhook-certgen/manifests/v1.6.2
 }
 repo_root=$(git rev-parse --show-toplevel)
 mkdir -p "${repo_root}/bin"
@@ -38,7 +40,7 @@ echo "INFO - Creating cluster ${CLUSTER_STAGING}"
 
 kind create cluster --name "${CLUSTER_STAGING}" \
 --image "kindest/node:${CLUSTER_VERSION}" \
---wait 5m
+--wait 5m --config kind-c2.yaml
 
 dockerid=$(docker ps --format "table {{.ID}}\t{{.Names}}" | grep $CLUSTER_STAGING | awk '{print $1}')
 addcerts $dockerid
@@ -48,7 +50,7 @@ echo "INFO - Creating cluster ${CLUSTER_PRODUCTION}"
 
 kind create cluster --name "${CLUSTER_PRODUCTION}" \
 --image "kindest/node:${CLUSTER_VERSION}" \
---wait 5m
+--wait 5m --config kind-c3.yaml
 
 dockerid=$(docker ps --format "table {{.ID}}\t{{.Names}}" | grep $CLUSTER_PRODUCTION | awk '{print $1}')
 addcerts $dockerid
